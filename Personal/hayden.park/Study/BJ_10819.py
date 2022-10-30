485ab474a39433a2584fe9d8b0468095a2276179
Hayden-Park <hayden.park@edwardsvacuum.com>
2022-10-30 09:49:56 +0900

    Update

--- Personal/hayden.park/Study/BJ_10819.py
+++ Personal/hayden.park/Study/BJ_10819.py
@@ -1,10 +1,48 @@
+# |A0 - A1| + |A1 - A2| + ... + |A(n-2) - A(n-1)| 의 최대값
+# 1. 내장 모듈 이용하기
+# from itertools import permutations
+
+# 2. 직접 순열 함수 만들기 (perm)
+def func_sum(n, numlist):
+    sum = 0
+    for i in range(n):
+        if i >= n-1:
+            break
+        else:
+            sum = sum + abs(numlist[i]-numlist[i+1])
+    return sum
+
+def perm(lst,n):
+    ret = []
+    if n > len(lst): return ret
+    if n==1:
+        for i in lst:
+            ret.append([i])
+    elif n>1:
+        for i in range(len(lst)):
+            temp = [i for i in lst] 
+            temp.remove(lst[i])
+            for p in perm(temp, n-1):
+                ret.append([lst[i]]+p)
+    return ret
+
 n = int(input())
 numlist = list(map(int, input().split()))
+allcase = perm(numlist,n)
+answer = 0
+for i in allcase:
+    max = func_sum(n,i)
+    if max >= answer:
+        answer = max
+    
+print(answer)
 
-sum = 0
-for i in range(n):
-    sum = sum + abs(numlist[i]-numlist[i+1])
-    if i >= n-1:
-        break
-print(sum)
-
+# 다른사람 정답 : 
+# n = int(input().strip())
+# case = sorted([int(v) for v in input().strip().split()])
+# case.sort()
+# def abs_sum(arr):
+#     return sum([abs(arr[i] - arr[i+1]) for i in range(len(arr) - 1)])
+# asc = [case[i // 2] if i % 2 == 0 else case[(i // 2 + 1) * (-1)] for i in range(len(case))]
+# desc = [case[i // 2] if i % 2 == 1 else case[(i // 2 + 1) * (-1)] for i in range(len(case))]
+# print(max([abs_sum([asc[-1]] + asc[:-1]), abs_sum([desc[-1]] + desc[:-1])]))
---
+++ Personal/hayden.park/Study/BJ_2798.py
@@ -0,0 +1,9 @@
+# 블랙잭
+# 5 21
+# 5 6 7 8 9
+
+# n, target = map(int, input().split())
+# card = sorted(list(map(int, input().split())))
+
+# print(card)
+
